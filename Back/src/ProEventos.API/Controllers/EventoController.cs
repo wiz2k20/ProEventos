eba76b65d7f8a3d2f835b1076deff4daa9b391bc
Wiz2k20git <marcbrcx@gmail.com>
2021-12-25 19:34:50 -0300

    ProEventos Sec2

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[] {
+                new Evento(){
+                    EventoId = 1,
+                    Tema = "teste",
+                    Local = "teste2",
+                    Lote = "teste3",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImageURL = "foto.png"
+                },
+                new Evento(){
+                    EventoId = 2,
+                    Tema = "teste",
+                    Local = "teste2",
+                    Lote = "teste3",
+                    QtdPessoas = 249,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImageURL = "foto.png"
+                }
+            };
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EventoController> _logger;
+
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+    }
+}
